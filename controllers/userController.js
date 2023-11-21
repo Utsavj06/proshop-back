e1abf859494cb8a0a89be7a9f32fdb5e881919db
Utsav <utsav96jaiswal@gmail.com>
2023-11-21 20:14:33 +0530

    remove cookies and send as response from backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,13 +49,14 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
-    generateToken(res, user._id);
+    const token =  generateToken(res, user._id);
 
     res.status(201).json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
+      token
     });
   } else {
     res.status(400);
