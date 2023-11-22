505b3573e5e6354845bf3aef6f6beb03e6eb57d1
Utsav <utsav96jaiswal@gmail.com>
2023-11-22 22:27:56 +0530

    getting new product detail in req to save in db

--- controllers/addProductController.js
+++ controllers/addProductController.js
@@ -1,5 +1,28 @@
-import asyncHandler from '../middleware/asyncHandler.js';
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
 
-export const addProduct = asyncHandler(async (req, res) => {
-    console.log(req, req.fields, req.files)
-  });
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
+});
+
+export const addProduct = async (req, res) => {
+  const proshop_products = "proshop_products";
+
+  try {
+    const uploadResult = await cloudinary.uploader.upload(
+      req.files.image.path,
+      {
+        folder: proshop_products,
+      }
+    );
+    console.log(uploadResult.secure_url);
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.status(200);
+};
