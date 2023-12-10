f686b12a458019b335b379064e6c389e35c8d9d3
Utsav <utsav96jaiswal@gmail.com>
2023-12-10 18:51:16 +0530

    remove unnecessary code

--- utils/UpdateOrCreateUserFromOauth.js
+++ utils/UpdateOrCreateUserFromOauth.js
@@ -1,15 +1,12 @@
 import User from  "../models/userModel.js";
 
 export const updateOrCreateUserFromOauth = async({ oauthUserInfo }) => {
-
-    console.log(oauthUserInfo)
     const {
         id: googleId,
         email,
         name
     } = oauthUserInfo
 
-    // const db = getDbConnection('react-auth-db')
     const existUser = await User.findOne({email})
 
     if(existUser) {
