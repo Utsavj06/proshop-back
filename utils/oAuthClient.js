fddff2a1b6c7f35c788158e6ebfc27e1812fbdb9
Utsav <utsav96jaiswal@gmail.com>
2023-12-10 18:49:28 +0530

    correct url redirection on selecting google account

--- utils/oAuthClient.js
+++ utils/oAuthClient.js
@@ -5,5 +5,5 @@ dotenv.config();
 export const oauthClient = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    'http://localhost:5000/auth/google/callback',
+    'https://proshop-back.onrender.com/auth/google/callback',
 )
