7b062ae5481a77aa37cd3657aa17f19af32e488c
Utsavj06 <85385280+Utsavj06@users.noreply.github.com>
2024-01-15 17:56:47 +0530

    Update getDeliveryOrderRoute.js to add route for manipulate delivery status

--- routes/getDeliveryOrderRoute.js
+++ routes/getDeliveryOrderRoute.js
@@ -1,6 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import { deliveryDetail } from '../controllers/deliveryOrder.js';
+import { deliveryDetail, deliverySuccess } from '../controllers/deliveryOrder.js';
 
 router.route('/order-delivery').get(deliveryDetail);
+router.route('/success-delivery').post(deliverySuccess);
+
 export default router;
